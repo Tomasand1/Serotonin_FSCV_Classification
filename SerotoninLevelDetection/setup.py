93227af541457088ff2f070eee8103fbf1f4f9ed
Tomas Andriuskevicius <ta821@ic.ac.uk>
2022-03-23 17:32:41 +0000

    Initialise repo with data science structure

---
+++ SerotoninLevelDetection/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Development of Neural Networks to distinguish between serotonin relase and non-release.',
+    author='Tomas Andriuskevicius',
+    license='',
+)
