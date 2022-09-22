d5cb6e627b35692e1c046503a3dfdc3f5b16a0d6
Tomas Andriuskevicius <ta821@ic.ac.uk>
2022-09-22 12:54:41 +0300

    Add dataset partitions to train, validation and test sets

---
+++ src/data/partition_dataset.py
@@ -0,0 +1,25 @@
+from sklearn.model_selection import train_test_split
+import numpy as np
+
+
+def get_dataset_partitions(data, labels, val_split=0.2, test_split=0.1):
+    # Use a utility from sklearn to split and shuffle your dataset.
+
+    train_ds, test_ds, train_labels, test_labels = train_test_split(
+        data, labels, test_size=0.1)
+    train_ds, val_ds, train_labels, val_labels = train_test_split(
+        train_ds, train_labels, test_size=0.2)
+
+    # Form np arrays of labels and features.
+    train_labels = np.array(train_labels)
+    val_labels = np.array(val_labels)
+    test_labels = np.array(test_labels)
+
+    train_features = np.array(train_ds)
+    val_features = np.array(val_ds)
+    test_features = np.array(test_ds)
+
+    print(train_features.shape)
+    print(train_labels.shape)
+
+    return train_features, train_labels, val_features, val_labels, test_features, test_labels
